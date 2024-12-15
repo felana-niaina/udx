d79449a4c3cbedfbbdbc5f2d2a847171188497dd
tiana261 <dev.tiana261@gmail.com>
2024-12-15 18:44:19 +0300

    Update database

--- udx.sql
+++ udx.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : sam. 14 déc. 2024 à 17:01
+-- Généré le : dim. 15 déc. 2024 à 16:40
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -32,6 +32,14 @@ CREATE TABLE `category` (
   `title` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`id`, `title`) VALUES
+(1, 'Non classé'),
+(2, 'Technology');
+
 -- --------------------------------------------------------
 
 --
@@ -46,18 +54,10 @@ CREATE TABLE `marketplace` (
   `keywords` varchar(200) NOT NULL,
   `clicks` varchar(100) NOT NULL,
   `ads` tinyint(1) DEFAULT NULL,
+  `picture` text NOT NULL,
   `userId` int(11) NOT NULL,
   `createdDate` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `marketplace`
---
-
-INSERT INTO `marketplace` (`id`, `title`, `description`, `price`, `keywords`, `clicks`, `ads`, `userId`, `createdDate`) VALUES
-(1, 'iPhone 13', 'Bonjour je vends mon iphone 13 pas cher', 0, 'iphone', '', NULL, 0, '2024-12-14'),
-(2, 'iPhone 16', 'iPhone 16 a vendre sous emballage', 600, 'iphone', '', NULL, 10, '2024-12-14'),
-(3, 'Tiana dev web', 'Tiana dev web freelance', 100, 'freelance', '', 0, 10, '2024-12-14');
 
 -- --------------------------------------------------------
 
@@ -81,7 +81,8 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `description`, `keywords`, `clicks`, `userId`, `categoryId`, `createdDate`) VALUES
-(1, 'Bonjour voici un post', 'Bonjour voici un post, ceci est un essai, merci', 'post', 1, 10, 0, '2024-12-14');
+(1, 'Bonjour voici un post', 'Bonjour voici un post, ceci est un essai, merci', 'post', 1, 10, 0, '2024-12-14'),
+(2, 'Check new post', 'Testing of new post form', '', 0, 8, 2, '2024-12-15');
 
 -- --------------------------------------------------------
 
@@ -124,15 +125,9 @@ CREATE TABLE `users` (
   `bio` varchar(255) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   `phone` varchar(50) DEFAULT NULL,
-  `location` varchar(255) DEFAULT NULL
+  `location` varchar(255) DEFAULT NULL,
+  `cover_photo` varchar(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`id`, `fullname`, `username`, `email`, `password`, `created_at`, `profileTitle`, `bio`, `url`, `phone`, `location`) VALUES
-(5, '', 'Tiana', 'dev.tiana2611@gmail.com', '$2y$10$egetVwgJwyaQX5KxgQLEP.43REZdga.kJpbnpW74Pnbv/BscI2MLK', '2024-12-14 07:09:18', NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Index pour les tables déchargées
@@ -151,6 +146,12 @@ ALTER TABLE `marketplace`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
@@ -165,19 +166,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `marketplace`
 --
 ALTER TABLE `marketplace`
-  MODIFY `id` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT pour la table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
