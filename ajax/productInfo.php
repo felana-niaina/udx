c1162c96ae65f826b2d612a1f6c5ea79bc4158b5
tiana261 <dev.tiana261@gmail.com>
2024-12-28 15:22:29 +0300

    view product pics

--- ajax/productInfo.php
+++ ajax/productInfo.php
@@ -8,8 +8,8 @@ $database = new DatabaseConnector();
 $con = $database->getConnection();
 $ResultsProvider = new MarketplaceResultsProvider($con);
 
-if(isset($_POST["productId"])){
-    echo json_encode($ResultsProvider->getProductById());
+if(isset($_POST["productId"]) && $_POST["productId"] > 0 ){
+    echo json_encode($ResultsProvider->getProductById($_POST["productId"]));
 } elseif(isset($_POST['listProduct'])) {
     echo json_encode($ResultsProvider->getProductList($_SESSION['user_id']));
 } else{
