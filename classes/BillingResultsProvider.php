b06883be56c8a14ddf4ce4e2864793e741ed2a9d
tiana261 <dev.tiana261@gmail.com>
2025-01-02 11:50:53 +0300

    nettoyage

--- classes/BillingResultsProvider.php
+++ classes/BillingResultsProvider.php
@@ -18,63 +18,6 @@ class BillingResultsProvider {
         $this->con = $con;
     }
 
-    // Méthode pour obtenir les résultats sous forme de HTML
-    public function getResultsHtml($page, $pageSize, $term) {
-        try {
-            if (!$this->con) {
-                throw new Exception("La connexion à la base de données a échoué.");
-            }
-
-            $fromLimit = ($page - 1) * $pageSize;
-
-            $query = $this->con->prepare("SELECT * 
-                                         FROM posts
-                                         WHERE title LIKE :term 
-                                         OR description LIKE :term 
-                                         OR keywords LIKE :term
-                                         ORDER BY clicks DESC
-                                         LIMIT :fromLimit, :pageSize");
-
-            $searchTerm = "%" . $term . "%";
-            $query->bindParam(":term", $searchTerm);
-            $query->bindParam(":fromLimit", $fromLimit, PDO::PARAM_INT);
-            $query->bindParam(":pageSize", $pageSize, PDO::PARAM_INT);
-            $query->execute();
-
-            $resultsHtml = "<div class='siteResults'>";
-
-            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-                $id = $row["id"];
-                $title = $row["title"];
-                $description = $row["description"];
-
-                // Truncation des champs title et description si nécessaire
-                $title = $this->trimField($title, 120);
-                $description = $this->trimField($description, 230);
-
-                // Ajouter le résultat au HTML
-                $resultsHtml .= "<div class='resultContainer'>
-                                    <h3 class='title'>
-                                            $title
-                                        </a>
-                                    </h3>
-                                    <span class='description'>$description</span>
-                                  </div>";
-            }
-
-            $resultsHtml .= "</div>"; // Fermer la div des résultats
-
-            return $resultsHtml;
-
-        } catch (PDOException $e) {
-            echo "Erreur de requête : " . $e->getMessage();
-            return "<p>Une erreur est survenue lors de l'affichage des résultats.</p>";
-        } catch (Exception $e) {
-            echo $e->getMessage();
-            return "<p>Une erreur est survenue lors de l'affichage des résultats.</p>";
-        }
-    }
-
     public function getBillingMethodByUser($userId){
         try {
             $query = $this->con->prepare("SELECT * 
