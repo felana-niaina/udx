da7a3d415803a77efdf673dd8598903b8f14d5b1
tiana261 <dev.tiana261@gmail.com>
2025-01-07 21:41:49 +0300

    Update notification template

--- settings.php
+++ settings.php
@@ -423,7 +423,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <style type="text/css">
         /* Corps de la page */
         body {
