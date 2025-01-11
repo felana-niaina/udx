eb87481df41fdec6d3812e78954357ccc3c10367
tiana261 <dev.tiana261@gmail.com>
2025-01-11 14:13:52 +0300

    Add message mobile

--- settings.php
+++ settings.php
@@ -871,6 +871,9 @@
               <li class="nav-item">
                 <a href="#ads" data-toggle="tab" class="nav-link has-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eur"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg></a>
               </li>
+              <li class="nav-item">
+                <a href="#messages" data-toggle="tab" class="nav-link has-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eur"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg></a>
+              </li>
             </ul>
           </div>
     
